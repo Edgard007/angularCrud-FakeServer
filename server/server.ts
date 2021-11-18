1ab2d1447bf8c6ac3858bc6ea98ddc76f5a37f6b
Francisco Palacios <edgard07hp@gmail.com>
2021-11-17 21:38:52 -0600

    🚀  API - Add Clients

--- server/server.ts
+++ server/server.ts
@@ -3,11 +3,36 @@ const server = jsonServer.create();
 const router = jsonServer.router('server/db.json');
 const middlewares = jsonServer.defaults();
 const db = require('./db.json');
+const fs = require('fs');
 
 const port = 3000; // ==> Confirgure the port
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+
+// || ==> Add the routes <== || //
+const readClient = () => {
+  const dbRaw = fs.readFileSync('./server/db.json');
+  const clients = JSON.parse(dbRaw).clientes;
+  return clients;
+};
+
+server.post('/clientes', (req: any, res: any) => {
+  const clients = readClient();
+
+  const request = req.body;
+
+  const client = (clients || []).filter((c: any) => c?.id === request?.id)[0];
+  if (client === undefined || client === null) {
+    res.send({
+      code: 0,
+      msg: 'Cliente agregado con éxito',
+    });
+    db.clientes.push(req.body);
+  } else {
+    res.status(500).send({ msg: 'Cliente ya existe' });
+  }
+});
 
 server.use(router);
 server.listen(port, () => {
