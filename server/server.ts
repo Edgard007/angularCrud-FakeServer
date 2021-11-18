dfabf7008815d0bdc46de2c797185aead1c70075
Francisco Palacios <edgard07hp@gmail.com>
2021-11-17 21:55:03 -0600

    🚀 API - Add Products

--- server/server.ts
+++ server/server.ts
@@ -34,6 +34,29 @@ server.post('/clientes', (req: any, res: any) => {
   }
 });
 
+const readProduct = () => {
+  const dbRaw = fs.readFileSync('./server/db.json');
+  const products = JSON.parse(dbRaw).productos;
+  return products;
+};
+
+server.post('/productos', (req: any, res: any) => {
+  const products = readProduct();
+
+  const request = req.body;
+
+  const product = (products || []).filter((c: any) => c?.id === request?.id)[0];
+  if (product === undefined || product === null) {
+    res.send({
+      code: 0,
+      msg: 'Producto agregado con éxito',
+    });
+    db.productos.push(req.body);
+  } else {
+    res.status(500).send({ msg: 'Producto ya existe' });
+  }
+});
+
 server.use(router);
 server.listen(port, () => {
   console.log(` || ==> JSON Server is running on port ${port} <== ||`);
